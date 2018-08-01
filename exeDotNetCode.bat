25a4424bbd4731c9b27a5968cf7e4b35ee8e3da0
kamleong <kamleong@users.noreply.github.com>
2018-08-02 07:34:46 +0800

    Update and rename c#.ps1.bat to exeDotNetCode.bat

--- exeDotNetCode.bat
+++ exeDotNetCode.bat
@@ -2,9 +2,9 @@
 rem set pshcmd="Add-Type -ReferencedAssemblies (\"System.Windows.Forms\") -Path \"%1\"; [myNameSpace.myMainClass]::Main(\"\")"
 rem ** To quote verbatim strings in Powershell, just use single quotes **
 set pshcmd=$WarningPreference='Continue'
-set pshcmd=%pshcmd%; $refAssy=('System.Web.Extensions','System.Windows.Forms')
+set pshcmd=%pshcmd%; $refAssy=@('System.Web.Extensions','System.Windows.Forms')
 set pshcmd=%pshcmd%; Add-Type -IgnoreWarnings -WarningAction 'Continue' -ReferencedAssemblies $refAssy -Path '%1'
-set pshcmd=%pshcmd%; $args=(''); [myNameSpace.myMainClass]::Main($args)
+set pshcmd=%pshcmd%; $args=( @() ); [myNameSpace.myMainClass]::Main($args)
 start notepad %1
 
 :@begin
