a8b49e5f6850432ea85f39148b714800a0f4594f
kamleong <kamleong@users.noreply.github.com>
2021-07-02 21:09:52 +0800

    Update ed.py

--- ed.py
+++ ed.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-# line editor by Lai KamLeong (2021)
+# line editor by QuoInsight (2021)
 
 import sys
-def printV(v) :
-  s = str(v).encode("utf8", errors='replace').decode(sys.stdout.encoding)
+def printV(v, end="\n") :
+  s = str(v).encode("utf8", errors='backslashreplace').decode(sys.stdout.encoding)
   print( s )
 #
 
 import os
 def loadFile(filepath) :
-  print(filepath)
+  printV(filepath)
   if os.path.isfile(filepath) :
-    with open(filepath, 'r') as f: lines=f.readlines() # txt=f.read()
+    with open(filepath, 'r',
+      encoding="utf8", errors="replace"
+    ) as f: lines=f.readlines() # txt=f.read()
   else :
     lines = []
   #
@@ -20,16 +22,16 @@ def loadFile(filepath) :
 
 def prnLine(lines, idx) :
   txt = ( lines[idx] if (len(lines)>idx) else "<null>\n" )
-  #print(str(idx+1) + ": " + txt, end='')
-  print(txt, end='')
+  #printV(str(idx+1) + ": " + txt, end='')
+  printV(txt, end='')
 #
 
 def cmdPrompt(pLines, pIdx) :
   global filepath, lines, idx
   cmd = input("^" + str(pIdx+1) + "> ").strip()
-  ## print("#" + cmd + "#")
+  ## printV("#" + cmd + "#")
   if (cmd=='?') :
-    print("""##> list of commands:
+    printV("""##> list of commands:
   ?          display this help message
   q          quit
   w          save to file/disk
@@ -47,24 +49,38 @@ def cmdPrompt(pLines, pIdx) :
   r          edit/replace current line
   d          delete current line
 """)
-    print("##> currentFile: " + filepath)
-    print("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
-  elif (cmd=='rr') :
-    lines = loadFile(filepath);
+    printV("##> currentFile: " + filepath)
+    printV("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
+  elif (cmd=='rr' or cmd.startswith('rr ')) :
+    confirmed = input("!! WARNING !! discard & reload? [Y/n]> ").strip().lower()
+    if not(confirmed=="" or confirmed=="y") : return cmdPrompt(pLines, pIdx)
+    if (cmd=='rr') :
+      pass
+    elif (cmd=='rr ?') :
+      filepath = input("filename> ").strip()
+    else :
+      filepath = cmd[3:]
+    #
+    try :
+      lines = loadFile(filepath);
+    except Exception as e:
+      printV("!! ERROR !! " + str(e))
+      lines = []
+    #
     idx = 0;  prnLine(lines, idx)
   elif (cmd=='ll') :
-    # print(''.join(lines))
+    # printV(''.join(lines))
     i = 0
     for line in lines :
-      i += 1;  print(str(i) + ": " + line.strip())
+      i += 1;  printV(str(i) + ": " + line.strip())
     #
   elif (cmd=='m') :
     for line in lines[idx:idx+10] :
-      print(line.strip())
+      printV(line.strip())
     #
   elif (cmd=='l') :
-    print("##> currentFile: " + filepath)
-    print("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
+    printV("##> currentFile: " + filepath)
+    printV("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
     prnLine(lines, idx)
   elif (cmd=='i' or cmd=='O') or (cmd=='a' or cmd=='o') :
     l = len(lines)
@@ -85,23 +101,23 @@ def cmdPrompt(pLines, pIdx) :
     l = len(lines)
     if (idx==0 and l<=1) :
       lines = []
-      print("##> all deleted")
+      printV("#> all deleted")
     elif (idx < l) :
       t = lines[idx]
       lines.pop(idx);
       if (idx > 0 and idx > l-2) :
-        print("##deleted##> " + t, end='')
+        printV("#deleted#> " + t, end='')
         idx -= 1
       else :
-        print("##deleted##> " + t, end='')
+        printV("#deleted#> " + t, end='')
       #
       prnLine(lines, idx)
     else :
       idx = l-1
     #
   elif (cmd=='w') :
-    print("##> currentFile: " + filepath)
-    print("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
+    printV("##> currentFile: " + filepath)
+    printV("##> lineIndex: " + str(idx+1) + "/" + str(len(lines)))
     if (filepath=="") : filepath = input("filename> ").strip()
     confirmed = input("!! WARNING !! save & overwrite? [Y/n/?]> ").strip().lower()
     if (confirmed=="?") : 
@@ -114,9 +130,9 @@ def cmdPrompt(pLines, pIdx) :
         f.writelines(lines)
         f.close()
       #
-      print("##> saved.")
-    except :
-      print("!! ERROR !! ", sys.exc_info()[0])
+      printV("##> saved.")
+    except Exception as e:
+      printV("!! ERROR !! " + str(e))
     #
   elif (cmd=='^' or cmd=='$' or cmd=='k' or cmd=='j' or cmd=='' or cmd.isdigit()) :
     if (cmd=='^') :
@@ -133,7 +149,7 @@ def cmdPrompt(pLines, pIdx) :
       if (i > 0):
         idx = i - 1
       else :
-        print("##> invalid command !")
+        printV("##> invalid command !")
         return cmd
       #
     #
@@ -144,7 +160,7 @@ def cmdPrompt(pLines, pIdx) :
   elif (cmd=='q!' or cmd=='Q') :
     pass
   else :
-    print("##> invalid command !")
+    printV("##> invalid command !")
   #
   return cmd
 #
@@ -152,7 +168,7 @@ def cmdPrompt(pLines, pIdx) :
 def main(argv) :
   _thisScript_ = sys.argv[0]  ## __file__
   global filepath, lines, idx
-  filepath = ( sys.argv[1] if (len(sys.argv)>1) else r"" );
+  filepath = ( sys.argv[1] if (len(sys.argv)>1) else r"B:\a.txt" );
   lines = loadFile(filepath);
   idx = 0;  prnLine(lines, idx)
   while ( cmdPrompt(lines, idx)!="q" ) :
